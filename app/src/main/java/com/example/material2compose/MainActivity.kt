a815b93eae7348f8e6a465a9a4db59abb4254fab
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-01-10 02:15:21 +0530

    Image painter added

--- app/src/main/java/com/example/material2compose/MainActivity.kt
+++ app/src/main/java/com/example/material2compose/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.material2compose.ui.theme.Material2ComposeTheme
 import kotlinx.coroutines.launch
 
@@ -23,6 +29,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val painter = painterResource(id = R.drawable.kermit)
+            val description = "Kermit in the snow"
+            val title = "Kermit is playing in the snow."
+
             Material2ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -30,7 +40,8 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    Greeting("Android")
 //                    AppBarExample()
-                    ColumnExample()
+//                    ColumnExample()
+                    ImageCardExample(painter, description, title)
                 }
             }
         }
@@ -113,12 +124,49 @@ fun ColumnExample() {
     }
 }
 
+@Composable
+fun ImageCardExample(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 16.sp)
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val painter = painterResource(id = R.drawable.kermit)
+    val description = "Kermit in the snow"
+    val title = "Kermit is playing in the snow."
     Material2ComposeTheme {
 //        Greeting("Android")
 //        AppBarExample()
-        ColumnExample()
+//        ColumnExample()
+        ImageCardExample(painter, description, title)
     }
 }
